68a2a74a8227c99a64e99ea5878d1badd63118ff
Christos Kormaris <chriskormaris@gmail.com>
2023-01-25 22:59:30 +0200

    Code maintenance
    Update `two_sat.py`

--- two_sat.py
+++ two_sat.py
@@ -27,15 +27,15 @@ k = len(set_of_clauses)  # k=5 clauses
 
 print('CNF clauses: ')
 print(set_of_clauses)
-print('')
+print()
 
 variable_assignment = {'x1': False, 'x2': False, 'x3': False}
 Y = []  # Y: how much different "A" is from "variable_assignment"
 
-print('initial variable assignment of truth variables: ' + str(variable_assignment))
-print('a satisfiable assignment of truth variables A: ' + str(A))
-print('Y: ' + str(Y))
-print('')
+print(f'Initial variable assignment of truth variables: {variable_assignment}')
+print(f'A satisfiable assignment of truth variables A: {A}')
+print(f'Y: {Y}')
+print()
 
 diff_keys = []
 not_true_clause_found = True
@@ -51,15 +51,15 @@ while not_true_clause_found and stages <= int(math.ceil(max_stages)):
         # check if clause is true
         true_found = False
         for i in range(len(clause)):
-        # for i in range(num_variables_per_clause):
+            # for i in range(num_variables_per_clause):
             value = False if '_' in clause[i] else True
             if value == variable_assignment[clause[i].replace('_', '')]:
                 true_found = True
 
         if true_found:
-            print('clause ' + str(j) + ': ' + str(clause) + ' -> True')
+            print(f'clause {j}: {clause} -> True')
         else:
-            print('clause ' + str(j) + ': ' + str(clause) + ' -> False')
+            print(f'clause {j}: {clause} -> False')
 
         # pick a variable randomly and change its value
         if not true_found:
@@ -69,10 +69,10 @@ while not_true_clause_found and stages <= int(math.ceil(max_stages)):
 
             # change the chosen variable from False to True or from True to False
             if not variable_assignment[variable_chosen]:
-                print('setting variable ' + str(variable_chosen) + ' to True!')
+                print(f'setting variable {variable_chosen} to True!')
                 variable_assignment[variable_chosen] = True
             else:
-                print('setting variable ' + str(variable_chosen) + ' to False!')
+                print(f'setting variable {variable_chosen} to False!')
                 variable_assignment[variable_chosen] = False
 
             # Skip the rest of this stage and go to the next stage.
@@ -82,18 +82,17 @@ while not_true_clause_found and stages <= int(math.ceil(max_stages)):
     diff_keys = [i for i in A if A[i] != variable_assignment[i]]
     Y.append(len(diff_keys))
 
-    print('current variable assignment of truth variables: ' + str(variable_assignment))
-    print('Y: ' + str(Y))
+    print(f'Current variable assignment of truth variables: {variable_assignment}')
+    print(f'Y: {Y}')
 
     stages += 1
     print('**********\n')
 
-
-print('stages done: ' + str(stages))
-print('max stages = ceil[(n ^ 2) * (1 + 4 * sqrt(2/3))] = ' + ' ceil[' + str(max_stages) + ']' + ' = ' + str(int(math.ceil(max_stages))))
-print('final variable assignment of truth variables: ' + str(variable_assignment))
-print('a satisfiable assignment of truth variables A: ' + str(A))
-print('Y: ' + str(Y))
+print(f'stages done: {stages}')
+print(f'max stages = ceil[(n ^ 2) * (1 + 4 * sqrt(2/3))] = ceil[{max_stages}] ~= {int(math.ceil(max_stages))}')
+print(f'Final variable assignment of truth variables: {variable_assignment}')
+print(f'A satisfiable assignment of truth variables A: {A}')
+print(f'Y: {Y}')
 
 if len(diff_keys) == 0:
     print('The algorithm has successfully found a solution for the 2-SAT problem!!!')
